2beca34ef140bc359d9febf84bf6ef137cfcd4be
A. Aryo <147596923+ZetsuRK@users.noreply.github.com>
2024-03-14 11:54:12 +0700

    Update kelompok_10_pemesanan_miexue.py

--- kelompok_10_pemesanan_miexue.py
+++ kelompok_10_pemesanan_miexue.py
@@ -44,3 +44,36 @@ class Pesanan:
     
     def __str__(self):
         return f"{self.id + 1}. {self.nama_menu.title()} - Rp{self.harga}"
+
+MIXUE_CABANG_MI = {
+    "Mixue Ice Cream": 5_000,
+    "Boba Shake": 16_000,
+    "Mi Sundae": 14_000,
+    "Mi Ganas": 11_000,
+    "Creamy Mango Boba": 22_000,
+}
+
+list_menu = list(MIXUE_CABANG_MI.items())
+
+menus = ""
+for i, menu in enumerate(list_menu):
+    menus += f"\n{i+1}. {menu[0]} - Rp{menu[-1]}"
+
+daftar_pesanan = DaftarPesanan()
+
+first = True
+while True:
+    pesanan = daftar_pesanan.tampilkan_menu(menus) if first else daftar_pesanan.tampilkan_menu(menus, message="Ada lagi? (ketik 'exit' untuk mengakhiri pesanan): ")
+    if pesanan == "exit":
+        break
+    elif pesanan not in range(0, len(list_menu)):
+        print("Pesanan yang dipilih tidak ada di Menu (input salah)")
+        continue
+    first = False
+    daftar_pesanan.tambahkan_pesanan(Pesanan(pesanan, list_menu[pesanan][0], list_menu[pesanan][-1]))
+
+if daftar_pesanan.length() <= 0:
+    print(f"Terima kasih telah berkunjung")
+else:
+    daftar_pesanan.rincian_pesanan(list_menu)
+    daftar_pesanan.total_pesanan()
