32f5e17b17011dbeefc6ae3a29f8d64c99c10a26
gerrymoeis <144098317+gerrymoeis@users.noreply.github.com>
2024-05-02 19:41:41 +0700

    Update kelompok_10_katakan_peta.py

--- kelompok_10_katakan_peta.py
+++ kelompok_10_katakan_peta.py
@@ -5,3 +5,18 @@
     2. Ahmad Aryobimo - 23091397151 (Notulis)
     3. Dea Ayu Novita Putri - 23091397173 (Editor)
 """
+
+class KatakanPeta():
+    def __init__(self):
+        self.daftarKota = {}
+        self.jumlahKota = 0
+    
+    def tampilkanPeta(self):
+        for kota in self.daftarKota:
+            print(f"{kota} -> {self.daftarKota[kota]}")
+        print(f"Jumlah Kota: {self.jumlahKota}")
+    
+    def tambahkanKota(self, kota):
+        if kota not in self.daftarKota:
+            self.daftarKota[kota] = {}
+            self.jumlahKota += 1
