cdaecfc4056244ae4c7b539aac6e9213e8673501
gerrymoeis <144098317+gerrymoeis@users.noreply.github.com>
2024-05-02 12:05:52 +0700

    Create kelompok_10_katakan_peta.py

---
+++ kelompok_10_katakan_peta.py
@@ -0,0 +1,7 @@
+"""
+    Kelompok 10_2023E - Kumpulan Tugas Struktur Data
+    
+    1. Gerry Moeis M.D.P - 23091397164 (Ketua)
+    2. Ahmad Aryobimo - 23091397151 (Notulis)
+    3. Dea Ayu Novita Putri - 23091397173 (Editor)
+"""
