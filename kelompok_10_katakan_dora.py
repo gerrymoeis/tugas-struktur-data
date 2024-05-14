56e309b962ef3e64a335639d53ed97a0d0d4e2ef
deaayunovitaputri <147597570+deaayunovitaputri@users.noreply.github.com>
2024-05-14 13:23:20 +0700

    Update kelompok_10_katakan_dora.py

--- kelompok_10_katakan_dora.py
+++ kelompok_10_katakan_dora.py
@@ -41,3 +41,29 @@ class KatakanPeta():
         if kota1 and kota2 in self.daftarKota:
             del self.daftarKota[kota1][kota2]
             del self.daftarKota[kota2][kota1]
+
+    def buatkanRute(self, start):
+        peta = {}
+
+        [distances, routes] = self.dijkstra(start)
+        for kota, jarak in distances.items():
+            peta[kota] = {}
+            peta[kota]["rute"] = self.route(distances, routes, start, kota)
+            peta[kota]["jarak"] = jarak
+
+        self.peta[start] = peta
+
+     def cariRuteTercepat(self, dari, ke):
+        dari = dari.title()
+        ke = ke.title()
+        if dari in self.daftarKota and ke in self.daftarKota:
+            print(f"\tDari {dari} ke {ke}, berjarak {self.peta[dari][ke]["jarak"]} km")
+            print(f"\tDengan rute: ")
+            for jalan, jarak in reversed(self.peta[dari][ke]["rute"].items()):
+                print("\t\t --->", jalan, jarak, "km")
+        else:
+            print(f"{dari} atau {ke} tidak berada di daftar kota")
+    
+    def route(self, distances, routes, start, target):
+        path = {}
+        city = target
